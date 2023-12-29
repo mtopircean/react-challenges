788c23ea3d2ee1a2397f18e764049a15ba3e96fe
Marius Topircean <mtopircean@yahoo.com>
2023-12-29 19:59:58 +0000

    Challenge 1

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from React
+
+function Sidebar() {
+    return (
+        <div>
+            <a>MyPhotos</a>
+            <a>MyIlustrations</a>
+            <a>MyPaintings</a>
+        </div>
+    );
+}
+
+export default Sidebar;
